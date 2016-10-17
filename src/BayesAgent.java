dacdba9fd2be271496bc0b173a22a3a08c8027bc
wimoMisterX <ran12us@gmail.com>
2016-10-17 10:19:42 +0800

    Added variable to mission tracker and cleaned up code

--- src/BayesAgent.java
+++ src/BayesAgent.java
@@ -8,86 +8,66 @@ public class BayesAgent implements Agent{
     private boolean spy;
     private int current_mission;
     private int total_failures;
-    private int mission_traitors;
+    private int total_wins;
     private String name;
-    private String current_leader;
     private ArrayList<String> players;
     private ArrayList<String> spy_list;
-    private ArrayList<String> all_mission_players;
-    private ArrayList<String> current_mission_players;
-    private ArrayList<String> previous_mission_votes;
+    private HistoryList<ArrayList<String>> current_mission_propositions;
 
     // History
     private AccusationList accusations;
-    private ArrayList<ArrayList<String>> mission_vote_list;
-    private ArrayList<ArrayList<String>> proposed_players_list;
-    private ArrayList<ArrayList<String>> players_mission_list;
-    private ArrayList<Integer> traitors_list;
+    private HistoryList<Integer> traitors_list;
+    private HistoryList<String> leader_list;
+    private HistoryList<ArrayList<String>> mission_vote_list;
+    private HistoryList<ArrayList<String>> players_mission_list;
+    private HistoryList<HistoryList<ArrayList<String>>> mission_propositions_list;
 
     public BayesAgent(){
-        accusations = new AccusationList();
         players = new ArrayList<String>();
         spy_list = new ArrayList<String>();
-        all_mission_players = new ArrayList<String>();
-        current_mission_players = new ArrayList<String>();
-        previous_mission_votes = new ArrayList<String>();
-
-        mission_vote_list = new ArrayList<ArrayList<String>>();
-        players_mission_list  = new ArrayList<ArrayList<String>>();
-        traitors_list = new ArrayList<Integer>();
+        accusations = new AccusationList();
+        mission_vote_list = new HistoryList<ArrayList<String>>();
+        players_mission_list  = new HistoryList<ArrayList<String>>();
+        traitors_list = new HistoryList<Integer>();
+        leader_list = new HistoryList<String>();
+        mission_propositions_list = new HistoryList<HistoryList<ArrayList<String>>>();
     }
 
     /**
-     * Reports the current status, inlcuding players name, the name of all players, the names of the spies (if known), the mission number and the number of failed missions
+     * Reports the current status, inlcuding players name, the name of all players,
+     * the names of the spies (if known), the mission number and the number of failed missions
      * @param name a string consisting of a single letter, the agent's names.
      * @param players a string consisting of one letter for everyone in the game.
-     * @param spies a String consisting of the latter name of each spy, if the agent is a spy, or n questions marks where n is the number of spies allocated; this should be sufficient for the agent to determine if they are a spy or not.
+     * @param spies a String consisting of the latter name of each spy,
+     * if the agent is a spy, or n questions marks where n is the number of spies allocated;
+     * this should be sufficient for the agent to determine if they are a spy or not.
      * @param mission the next mission to be launched
      * @param failures the number of failed missions
      * */
     public void get_status(String name, String players, String spies, int mission, int failures){
         this.name = name;
         this.players = new ArrayList<String>(Arrays.asList(players.split(string_delimenator)));
-        spy = spies.indexOf(name) != -1; // Checking if we are a spy
-        spies = spy ? spies : "";
-        spy_list = new ArrayList<String>(Arrays.asList(spies.split(string_delimenator)));
         current_mission = mission - 1;
-        total_failures = failures;
-        proposed_players_list = new ArrayList<ArrayList<String>>();
+        spy = spies.indexOf(name) != -1; // Checking if we are a spy
+        if (spy){
+            spy_list = new ArrayList<String>(Arrays.asList(spies.split(string_delimenator)));
+            total_wins = failures;
+            total_failures = current_mission - total_wins;
+        }else{
+            spy_list = new ArrayList<String>(Arrays.asList("".split(string_delimenator)));
+            total_failures = failures;
+            total_wins = current_mission - total_failures;
+        }
+        mission_propositions_list.add(current_mission-1, current_mission_propositions);
+        current_mission_propositions = new HistoryList<ArrayList<String>>();
     }
 
-    private String get_lowest_key(HashMap<String, Integer> map){
-        String key = "";
-        int lowest_value = -1;
-        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Integer> pair = it.next();
-            if(lowest_value == -1 || pair.getValue() < lowest_value){
-                key = pair.getKey();
-                lowest_value = pair.getValue();
-            }
-        }
-        return key;
-    }
-
-    private String get_highest_key(HashMap<String, Integer> map){
-        String key = "";
-        int highest_value = -1;
-        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Integer> pair = it.next();
-            if(highest_value == -1 || pair.getValue() > highest_value){
-                key = pair.getKey();
-                highest_value = pair.getValue();
-            }
-        }
-        return key;
-    }
 
     /**
      * Nominates a group of agents to go on a mission.
      * If the String does not correspond to a legitimate mission (<i>number</i> of distinct agents, in a String),
-     * a default nomination of the first <i>number</i> agents (in alphabetical order) will be used, as if this was what the agent nominated.
+     * a default nomination of the first <i>number</i> agents (in alphabetical order) will be used,
+     * as if this was what the agent nominated.
      * @param number the number of agents to be sent on the mission
      * @return a String containing the names of all the agents in a mission
      * */
@@ -124,16 +104,11 @@ public class BayesAgent implements Agent{
      * @param mission a String containing the names of all the agents in the mission
      **/
     public void get_ProposedMission(String leader, String mission){
-        current_leader = leader;
-        all_mission_players = new ArrayList<String>(Arrays.asList(mission.split(string_delimenator)));
-        proposed_players_list.add(all_mission_players);
-    }
-
-    private boolean spy_in_team(ArrayList<String> team){
-        for (String player : spy_list){
-            if (team.contains(player)) return true;
-        }
-        return false;
+        leader_list.add(current_mission, leader);
+        current_mission_propositions.add(
+            current_mission_propositions.get_next_key(),
+            new ArrayList<String>(Arrays.asList(mission.split(string_delimenator)))
+        );
     }
 
     /**
@@ -147,12 +122,13 @@ public class BayesAgent implements Agent{
             // Last mission voting up since the resistance would
             if (current_mission == 5) return false;
             // Vote up for a mission with a spy
-            if (spy_in_team(all_mission_players)) return true;
+            if (spy_in_team(current_mission_propositions.get_latest_value())) return true;
             // Voting strongly about this team because it's size 3
-            if (all_mission_players.size() == 3) return all_mission_players.contains(name);
+            if (current_mission_propositions.get_latest_value().size() == 3)
+                return current_mission_propositions.get_latest_value().contains(name);
         }
         // Approving my own mission selection
-        if (current_leader == name) return true;
+        if (leader_list.get_latest_value() == name) return true;
         // Voting last mission to avoid failure
         if (current_mission == 5) return true;
         return false;
@@ -163,8 +139,7 @@ public class BayesAgent implements Agent{
      * @param yays the names of the agents who voted for the mission
      **/
     public void get_Votes(String yays){
-        previous_mission_votes = new ArrayList<String>(Arrays.asList(yays.split(string_delimenator)));
-        mission_vote_list.add(previous_mission_votes);
+        mission_vote_list.add(current_mission, new ArrayList<String>(Arrays.asList(yays.split(string_delimenator))));
     }
 
     /**
@@ -173,8 +148,7 @@ public class BayesAgent implements Agent{
      * @param mission the Agents being sent on a mission
      **/
     public void get_Mission(String mission){
-        current_mission_players = new ArrayList<String>(Arrays.asList(mission.split(string_delimenator)));
-        players_mission_list.add(current_mission_players);
+        players_mission_list.add(current_mission, new ArrayList<String>(Arrays.asList(mission.split(string_delimenator))));
     }
 
     /**
@@ -183,12 +157,13 @@ public class BayesAgent implements Agent{
      **/
     public boolean do_Betray(){
         // If the mission has less than 30% of the players betraying is risky
-        if((double) current_mission_players.size() / players.size() < 0.3){
+        if((double) players_mission_list.get_latest_value().size() / players.size() < 0.3){
             return false;
         }
 
         // Linear increase of probably of betrayal throughout the game if we are a spy
-        // We want to betray the mission BUT earlier in the game it is more risky to do so as others might see a pattern
+        // We want to betray the mission BUT earlier in the game
+        // it is more risky to do so as others might see a pattern
         // Special case if we have 2 failed missions; betray as we will win
         int num_missions = 5;
         return spy && ((double) current_mission / num_missions) > Math.random() || total_failures == 2;
@@ -199,10 +174,8 @@ public class BayesAgent implements Agent{
      * @param traitors the number of people on the mission who chose to betray (0 for success, greater than 0 for failure)
      **/
     public void get_Traitors(int traitors){
-        traitors_list.add(traitors);
-        mission_traitors = traitors;
+        traitors_list.add(current_mission, traitors);
     }
-
 
     /**
      * Optional method to accuse other Agents of being spies.
@@ -224,8 +197,8 @@ public class BayesAgent implements Agent{
         }
 
         // If the last mission had n players and n betrayals, accuse all of the players
-        if(!spy && current_mission_players.size() == mission_traitors){
-            return String.join("", current_mission_players);
+        if(!spy && players_mission_list.get_latest_value().size() == traitors_list.get_latest_value()){
+            return String.join("", players_mission_list.get_latest_value());
         }
         return "";
     }
@@ -237,6 +210,45 @@ public class BayesAgent implements Agent{
      * */
     public void get_Accusation(String accuser, String accused){
         accusations.add_accusation(accuser, accused, string_delimenator);
+    }
+
+    //////////////////////
+    // Helper Functions //
+    /////////////////////
+
+    private String get_lowest_key(HashMap<String, Integer> map){
+        String key = "";
+        int lowest_value = -1;
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> pair = it.next();
+            if(lowest_value == -1 || pair.getValue() < lowest_value){
+                key = pair.getKey();
+                lowest_value = pair.getValue();
+            }
+        }
+        return key;
+    }
+
+    private String get_highest_key(HashMap<String, Integer> map){
+        String key = "";
+        int highest_value = -1;
+        Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> pair = it.next();
+            if(highest_value == -1 || pair.getValue() > highest_value){
+                key = pair.getKey();
+                highest_value = pair.getValue();
+            }
+        }
+        return key;
+    }
+
+    private boolean spy_in_team(ArrayList<String> team){
+        for (String player : spy_list){
+            if (team.contains(player)) return true;
+        }
+        return false;
     }
 
 }
---
+++ src/HistoryList.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HistoryList<T> {
+
+    private HashMap<Integer, T> main;
+    private int last_key;
+
+    public HistoryList(){
+        main = new HashMap<Integer, T>();
+        last_key = 0;
+    }
+
+    public void add(int k, T s){
+        main.put(k, s);
+        last_key = k;
+    }
+
+    public T get_value_for_key(int k){
+        return main.get(k);
+    }
+
+    public T get_latest_value(){
+        return main.get(last_key);
+    }
+
+    public int get_next_key(){
+        return last_key + 1;
+    }
+}
